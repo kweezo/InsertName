5bb24a6d44e971a91bae67155c6a76d811774251
Jakob Tadej Vrtacnik <jakobtadejv@gmail.com>
2024-03-09 17:38:46 +0100

    linux build system finished

---
+++ CMakeFiles/InsertName.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/InsertName.dir/src/main.cpp.o"
+  "CMakeFiles/InsertName.dir/src/main.cpp.o.d"
+  "bin/InsertName"
+  "bin/InsertName.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/InsertName.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
