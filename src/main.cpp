b80c3440cd166e25b60f9c0dfddc0ed46a1420c3
Skelci <nikomaren2@gmail.com>
2024-03-09 23:16:53 +0100

    finally working build system

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,26 @@
-#include <iostream>
+#include <glfw3.h>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    if (!glfwInit()) {
+        return -1;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(1920, 1080, "Hello World", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
