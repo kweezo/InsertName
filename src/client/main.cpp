822049e0177aea0da5d94ed4d9ed27a84fec66b1
Skelci <nikomaren2@gmail.com>
2024-03-10 19:48:36 +0100

    neki

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -6,9 +6,6 @@
 int main(){
     Settings settings;
     ReadSettings(settings, "../src/settings.bin");
-    #include <iostream>
-    std::cout << "Width: " << settings.width << std::endl;
-    std::cout << "Height: " << settings.height << std::endl;
 
     Window::CreateWindowContext(settings.width, settings.height, "Vulkan");
     Renderer::InitRenderer();
