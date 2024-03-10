5c9161f942214cf964a740d282110041bd3e0a41
Jakob Tadej Vrtacnik <jakobtadejv@gmail.com>
2024-03-10 10:10:53 +0100

    Some basic file structure

--- CMakeFiles/InsertName.dir/src/main.cpp.o.d
+++ CMakeFiles/InsertName.dir/src/main.cpp.o.d
@@ -155,4 +155,6 @@ CMakeFiles/InsertName.dir/src/main.cpp.o: \
  /usr/include/c++/11/bits/locale_facets.tcc \
  /usr/include/c++/11/bits/basic_ios.tcc \
  /usr/include/c++/11/bits/ostream.tcc /usr/include/c++/11/istream \
- /usr/include/c++/11/bits/istream.tcc
+ /usr/include/c++/11/bits/istream.tcc /usr/include/GLFW/glfw3.h \
+ /usr/include/GL/gl.h /usr/include/GL/glext.h \
+ /usr/include/KHR/khrplatform.h
