0159572314ffc76c1707f32ff75fbda3a3686272
Niko Maren R1C <niko.maren@students.vegova.si>
2024-03-26 07:08:54 +0100

    nvm

--- src/client/network/NetworkMangaer.cpp
+++ src/client/network/NetworkMangaer.cpp
@@ -9,8 +9,7 @@ NetworkManager::NetworkManager(const std::string& serverIP, int port)
         WSAData data;
         WORD ver = MAKEWORD(2, 2);
         int wsResult = WSAStartup(ver, &data);
-        if (wsResult != 0)
-        {
+        if (wsResult != 0) {
             std::cerr << "Can't start Winsock, Err #" << wsResult << '\n';
         }
     #endif
@@ -27,8 +26,7 @@ NetworkManager::~NetworkManager() {
 bool NetworkManager::connectToServer() {
     // Create socket
     sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock == INVALID_SOCKET)
-    {
+    if (sock == INVALID_SOCKET) {
         std::cerr << "Can't create socket, Err #" << WSAGetLastError() << '\n';
         return false;
     }
@@ -41,8 +39,7 @@ bool NetworkManager::connectToServer() {
 
     // Connect to server
     int connResult = connect(sock, (sockaddr*)&hint, sizeof(hint));
-    if (connResult == SOCKET_ERROR)
-    {
+    if (connResult == SOCKET_ERROR) {
         std::cerr << "Can't connect to server, Err #" << WSAGetLastError() << '\n';
         return false;
     }
@@ -55,23 +52,19 @@ bool NetworkManager::sendAndReceiveData() {
     char buf[4096];
     std::string userInput;
 
-    do
-    {
+    do {
         // Prompt the user for some text
         std::cout << "> ";
         std::getline(std::cin, userInput);
 
-        if (userInput.size() > 0) // Make sure the user has typed in something
-        {
+        if (userInput.size() > 0) {
             // Send the text
             int sendResult = send(sock, userInput.c_str(), userInput.size() + 1, 0);
-            if (sendResult != SOCKET_ERROR)
-            {
+            if (sendResult != SOCKET_ERROR) {
                 // Wait for response
                 memset(buf, 0, 4096);
                 int bytesReceived = recv(sock, buf, 4096, 0);
-                if (bytesReceived > 0)
-                {
+                if (bytesReceived > 0) {
                     // Echo response to console
                     std::cout << "SERVER> " << std::string(buf, 0, bytesReceived) << '\n';
                 }
