71fd2fba32116bc3f751e17193a0cef65089daf7
Niko Maren R1C <niko.maren@students.vegova.si>
2024-03-27 07:28:00 +0100

    addet function stickMessege()

--- src/client/account/UserManager.cpp
+++ src/client/account/UserManager.cpp
@@ -1,14 +1,20 @@
 #include "UserManager.hpp"
 
-UserManager::UserManager() {
 
+UserManager::UserManager() : NetworkManager("127.0.0.1", 12345) {
+    connectToServer();
 }
 
 UserManager::~UserManager() {
 
 }
 
+std::string UserManager::stickMessage(const std::string &username, const std::string &password) {
+    return (char)0 + username + (char)0 + password;
+}
+
 bool UserManager::registerUser(const std::string &username, const std::string &password) {
+
     return false;
 }
 
--- src/client/account/UserManager.hpp
+++ src/client/account/UserManager.hpp
@@ -1,12 +1,17 @@
 #pragma once
 
+#include "../network/NetworkManager.hpp"
+
 #include <string>
 
-class UserManager {
+class UserManager : public NetworkManager {
 public:
     UserManager();
     ~UserManager();
 
     bool registerUser(const std::string& username, const std::string& password);
     bool loginUser(const std::string& username, const std::string& password);
+
+private:
+    std::string stickMessage(const std::string& username, const std::string& password);
 };
