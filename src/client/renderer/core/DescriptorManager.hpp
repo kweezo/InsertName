35d174453bce88ad58239beabb1f0a67399463bd
Jakob Tadej Vrtacnik <jakobtadejv@gmail.com>
2024-04-14 11:01:25 +0200

    the unfucking

--- src/client/renderer/core/DescriptorManager.hpp
+++ src/client/renderer/core/DescriptorManager.hpp
@@ -9,17 +9,33 @@
 
 namespace renderer{
 
-typedef struct DescriptorBatch{
+typedef struct {
     VkDescriptorSetLayout layout;
     VkDescriptorPool pool;
     std::vector<VkDescriptorSet> sets;
-} DescriptorBatch;
+}DescriptorBatch;
+
+typedef struct {
+    uint32_t layoutIndex;
+    uint32_t setCount;
+}DescriptorBatchInfo;
+
+typedef struct{
+    uint32_t batchIndex;
+    uint32_t index;
+}DescriptorHandle;
 
 class DescriptorManager{
 public:
     static void Initialize(std::vector<VkDescriptorSetLayoutCreateInfo> layoutInfos);
-    static void CreateDescriptors(std::vector<uint32_t> layoutIndexes);
+    static std::vector<DescriptorHandle> CreateDescriptors(std::vector<DescriptorBatchInfo> batchInfos); //TODO implement support for othre VkDescriptorTypes
     static void CreateDescriptor(uint32_t layoutIndex); //TODO implement once I figure out how to batch this mf
+
+    static VkDescriptorSet GetDescriptorSet(DescriptorHandle handles);
+
+    static std::vector<VkDescriptorSetLayout>* GetLayouts();
+
+    static void Cleanup();
 private:
     static std::vector<VkDescriptorSetLayout> layouts;
     static std::vector<DescriptorBatch> batches;
