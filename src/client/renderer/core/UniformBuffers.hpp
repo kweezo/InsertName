99ae0eee590ea4d0c569fffd60e6ff2d728cb6f1
Jakob Tadej Vrtacnik <jakobtadejv@gmail.com>
2024-04-15 20:35:46 +0200

    Revert "fixed the version clusterfuck that was upon us"
    This reverts commit 26511c8bce43727e06f9c30e1defd15e0d804901.

---
+++ src/client/renderer/core/UniformBuffers.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+
+#include <vector>
+#include <stdexcept>
+
+#include <vulkan/vulkan.h>
+
+#include "DataBuffer.hpp"
+#include "DescriptorManager.hpp"
+
+class UniformBuffers{
+public:
+
+private:
+};
