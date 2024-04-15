cdf2497eda5ef44f69ecf0aadb67c0fa62bfe737
Jakob Tadej Vrtacnik <jakobtadejv@gmail.com>
2024-04-15 21:03:45 +0200

    jebem ti mater culling me skoz zajebava

--- src/client/renderer/core/GraphicsPipeline.cpp
+++ src/client/renderer/core/GraphicsPipeline.cpp
@@ -30,7 +30,7 @@ GraphicsPipeline::GraphicsPipeline(VkPipelineVertexInputStateCreateInfo vertexIn
     rasterizer.rasterizerDiscardEnable = VK_FALSE;
     rasterizer.polygonMode = polygonMode;
     rasterizer.lineWidth = 1.0f;
-    rasterizer.cullMode = VK_CULL_MODE_BACK_BIT;
+    rasterizer.cullMode = VK_CULL_MODE_NONE;
     rasterizer.frontFace = VK_FRONT_FACE_COUNTER_CLOCKWISE;
     rasterizer.depthBiasEnable = VK_FALSE;
 
