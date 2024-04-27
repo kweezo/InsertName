b6a8ae6f578d3a38ef3c30e3627feffee42fbc65
--get <nikomaren2@gmail.com>
2024-04-27 10:50:02 +0200

    fixed build errors

--- src/client/account/NetworkManager.cpp
+++ src/client/account/NetworkManager.cpp
@@ -136,31 +136,3 @@ std::string NetworkManager::sendData(const std::string& message) {
     return "";
 }
 */
-template <typename T>
-std::string NetworkManager::sendData(unsigned char identifier, const T& data) {
-    // Convert the data to binary
-    const char* binaryData = reinterpret_cast<const char*>(&data);
-    std::string binaryString(binaryData, binaryData + sizeof(T));
-
-    // Prepend the identifier
-    binaryString.insert(binaryString.begin(), identifier);
-
-    // Send the binary data
-    int sendResult = SSL_write(ssl, binaryString.c_str(), binaryString.size());
-    if (sendResult <= 0) {
-        // handle error
-        return "";
-    }
-
-    // Receive the response
-    int bufferSize = Settings::GetInstance().messageBufferSize;
-    char buf[bufferSize];
-    memset(buf, 0, bufferSize);
-    int bytesReceived = SSL_read(ssl, buf, bufferSize);
-    if (bytesReceived > 0) {
-        std::string response(buf, 0, bytesReceived);
-        return response;
-    }
-    
-    return "";
-}
--- src/client/account/NetworkManager.hpp
+++ src/client/account/NetworkManager.hpp
@@ -40,3 +40,32 @@ private:
     SSL_CTX* ctx;
     SSL* ssl;
 };
+
+template <typename T>
+std::string NetworkManager::sendData(unsigned char identifier, const T& data) {
+    // Convert the data to binary
+    const char* binaryData = reinterpret_cast<const char*>(&data);
+    std::string binaryString(binaryData, binaryData + sizeof(T));
+
+    // Prepend the identifier
+    binaryString.insert(binaryString.begin(), identifier);
+
+    // Send the binary data
+    int sendResult = SSL_write(ssl, binaryString.c_str(), binaryString.size());
+    if (sendResult <= 0) {
+        // handle error
+        return "";
+    }
+
+    // Receive the response
+    int bufferSize = Settings::GetInstance().messageBufferSize;
+    char buf[bufferSize];
+    memset(buf, 0, bufferSize);
+    int bytesReceived = SSL_read(ssl, buf, bufferSize);
+    if (bytesReceived > 0) {
+        std::string response(buf, 0, bytesReceived);
+        return response;
+    }
+    
+    return "";
+}
